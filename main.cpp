2aebb58b064d17683dd82e08dc3db69ad9c6fa7f
Joseph Maa <josephgmaa@berkeley.edu>
2023-01-03 12:58:02 -0800

    Add command line arguments and basic write functionality

--- main.cpp
+++ main.cpp
@@ -1,18 +1,40 @@
-#include <QtDataVisualization>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main(int argc, char **argv)
-{
-    qputenv("QSG_RHI_BACKEND", "opengl");
-    QGuiApplication app(argc, argv);
+int main (int argc, char**argv) {
+    // Grab the command line arguments for parsing the input file.
+    std::vector<std::string> args(argv + 1, argv + argc);
+    std::string infname, outfname;
 
-    Q3DScatter scatter;
-    scatter.setFlags(scatter.flags() ^ Qt::FramelessWindowHint);
-    QScatter3DSeries *series = new QScatter3DSeries;
-    QScatterDataArray data;
-    data << QVector3D(0.5f, 0.5f, 0.5f) << QVector3D(-0.3f, -0.5f, -0.4f) << QVector3D(0.0f, -0.3f, 0.2f);
-    series->dataProxy()->addItems(data);
-    scatter.addSeries(series);
-    scatter.show();
+    // Loop over command-line args.
+    for (auto i = args.begin(); i != args.end(); ++i) {
+        if (*i == "-h" || *i == "--help") {
+            std::cout << "Generates a qml file from an xyz formatted chemical." << std::endl;
+            return 0;
+        } else if (*i == "-i") {
+            infname = *++i;
+        } else if (*i == "-o") {
+            outfname = *++i;
+        }
+    }
 
-    return app.exec();
+    std::ofstream qml_file;
+    qml_file.open(outfname);
+    // Header for qml files. 
+    const char * header = R"(import QtQuick 2.1
+
+Item {
+    //! [1]
+    property alias model: dataModel
+    property alias modelTwo: dataModelTwo
+    property alias modelThree: dataModelThree
+    //! [1])";
+    qml_file << header;
+
+    // TODO: Read in the input xyz molecular file format.
+
+    qml_file.close();
+    return 0;
 }
